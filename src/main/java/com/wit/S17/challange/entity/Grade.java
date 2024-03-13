28d6578e1793fb231fd18a8cd5e1d441958aab08
Bilge <134232792+goksubilge@users.noreply.github.com>
2024-03-13 10:36:15 +0300

    add mission 1

---
+++ src/main/java/com/wit/S17/challange/entity/Grade.java
@@ -0,0 +1,14 @@
+package com.wit.S17.challange.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Grade {
+    private int coefficient;
+    private String note;
+}
