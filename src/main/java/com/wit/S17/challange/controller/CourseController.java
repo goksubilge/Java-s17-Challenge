146c87ede930b06ebadb149a02d6ada774ecba10
Bilge <134232792+goksubilge@users.noreply.github.com>
2024-03-17 11:02:43 +0300

    add GetMapping from mission 2

--- src/main/java/com/wit/S17/challange/controller/CourseController.java
+++ src/main/java/com/wit/S17/challange/controller/CourseController.java
@@ -5,11 +5,11 @@ import com.wit.S17.challange.entity.CourseGPA;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController // create CourseController 's instance
 @RequestMapping("/courses")
@@ -32,4 +32,25 @@ public class CourseController {
         this.mediumGPA = mediumGPA;
         this.highGPA = highGPA;
     }
+
+    @GetMapping("/")
+    public List<Course> getCourses(){return courses;}
+
+    @GetMapping("/{name}")
+    public Course findCourse(@PathVariable String name) {
+        // courses.stream().filter(course -> course.getName().equals(name)).findFirst();
+        // 1.) bu logic i yazdık.
+        // isim UNIC olacak  yüzden findFirst()
+        //  2.) findFirst hangi tip dönüyor diye baktık, uyarı verdi zaten. Optional type.
+        //  3.) bi kırılma noktası oluyor Optional 'da ya break yapacaksın ya da if ile koşulunu yazacaksın.
+        Optional<Course> optCourse = courses.stream().filter(course -> course.getName().equals(name)).findFirst();
+        if(optCourse.isPresent()){
+            return optCourse.get();
+        } else {
+            //TODO throw course not found exception
+            return null;
+        }
+    }
+    // NOT: dönüş değerlerim hiç bir zaman entity class isimlerime aynı olmamalı. bunun için bir dto package tanımlamalıyım. yani "public Course .." burada Course u gidip dto --> CourseResponse class 'ında tanımlayacağım.
+
 }
